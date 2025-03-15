cafed5eb25508830cb5f2f5d617108e8c7f78753
Sam <ssamsanya3@gmail.com>
2025-03-15 12:38:53 +0300

    arrayfilteringMethod

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
